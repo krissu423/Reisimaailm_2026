fbdd107db9b1805e9069a560c153dce7dcfa957e
krissu423 <125663006+krissu423@users.noreply.github.com>
2026-04-06 23:57:15 +0300

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from '@vitejs/plugin-react'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Reisimaailm_2026/' : '/',
   plugins: [
     react(),
     tailwindcss(),
